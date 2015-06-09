596aeef070a76bbc750b9c9c9e5b89176ee917bb
Olga Kim <aver.kev@gmail.com>
2015-06-09 15:29:35 +0300

    test

--- setup/settings.js
+++ setup/settings.js
@@ -1,7 +1,7 @@
 var Experigen =  {
 	settings: {
 
-		experimentName: "Default", // use only A-Z, a-z, 0-9
+		experimentName: "predict", // use only A-Z, a-z, 0-9
 		
 		databaseServer: "http://db.phonologist.org/",
 
