7f0b2bbc43cd9415e4000724eb1caaff408c378d
Olga Kim <aver.kev@gmail.com>
2015-06-09 16:18:09 +0300

    test

--- setup/design.js
+++ setup/design.js
@@ -1,8 +1,5 @@
 Experigen.initialize = function () {
 
 	this.addStaticScreen("intro.ejs");
-	this.addStaticScreen("getgoing.ejs");
-	this.addStaticScreen("demographic.ejs");
-	this.addStaticScreen("finalthanks.ejs");
-	
+
 }
