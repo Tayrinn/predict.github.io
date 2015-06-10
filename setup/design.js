b7e68a9e415086eb491ef10c4bbb63220a464993
Olga Kim <aver.kev@gmail.com>
2015-06-10 13:53:21 +0300

    stimulus

--- setup/design.js
+++ setup/design.js
@@ -1,5 +1,14 @@
 Experigen.initialize = function () {
 
+    var frames = this.resource("frames");
+    var block1 = []
+            .concat(frames)
+			.pairWith("view","stimulus.ejs")
+			;
+
 	this.addStaticScreen("intro.ejs");
+    this.addBlock(block1);
+    this.addStaticScreen("demographic.ejs");
+	this.addStaticScreen("finalthanks.ejs");
 
 }
