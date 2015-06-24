8b4742a5a4b5c8f140989e2923fa138358e534d8
Olga Kim <aver.kev@gmail.com>
2015-06-24 21:54:07 +0300

    new columns

--- setup/design.js
+++ setup/design.js
@@ -4,6 +4,8 @@ Experigen.initialize = function () {
     var words = []
     var result = []
     var right_answer = []
+	var original = []
+	var number = []
 	var order_max = 4
 	
 	var order = Math.floor(Math.random() * (order_max))
@@ -14,20 +16,29 @@ Experigen.initialize = function () {
         //items[i].text = items[i].id + ". " + items[i].text
 		words.push(items[i].text.split(/\s*[ ]+\s*/))
         result.push("Введите первое слово")
+		original.push(items[i].text)
 		right_answer.push(words[count][0])
-        result.push(words[count][0])
+        number.push(1)
+		
+		result.push(words[count][0])
         for (var j = 1; j < words[count].length - 1; j++) {
             right_answer.push(words[count][j])
+			number.push(j + 1)
+			original.push(items[i].text)
 			var new_result = result[result.length - 1] + " " + words[count][j]
             result.push(new_result)
         }
-        //right_answer.push(i + ". __end__")
+		original.push(items[i].text)
+		number.push(words[count].length)
+        right_answer.push(words[count][length - 1])
         //result[result.length - 1] += "<br> Нажмите enter для продолжения..."
     }    
     
     items = items.pairWith("item", result)
     var block1 = [].concat(items)
             .pairWith("right_answer", right_answer)
+            .pairWith("original", original)
+            .pairWith("number", number)
 			.pairWith("view","stimulus.ejs")
 			;
 
